7a777aa3931c5e146fc1251b3e15f13363e0b41c
Jusqu123 <jusquu123@gmail.com>
2016-01-13 13:37:37 +0200

    tehtävä 4

---
+++ demo2harjoitus4/demo2harjoitus4/Program.cs
@@ -0,0 +1,29 @@
+﻿
+// Tee ohjelma, jossa kysytään käyttäjältä tämän ikä. Jos ikä on alle 18 vuotta, tulosta "alaikäinen", jos se on 18-65 vuotta, tulosta "aikuinen", muussa tapauksessa tulosta "seniori". 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo1harjoitus4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int luku;
+            Console.Write("Anna ikasi");
+            luku = int.Parse(Console.ReadLine());
+            if (luku < 18)
+                Console.WriteLine("alaikainen");
+
+            else if (luku >= 18 && luku <= 65)
+                Console.WriteLine("aikuinen");
+
+            else if (luku >= 65)
+                Console.WriteLine("seniori");
+        }
+    }
+}
