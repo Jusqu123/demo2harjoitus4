df705599add92bfec8075f62c1fbdfe0061b914b
Jusqu123 <jusquu123@gmail.com>
2016-01-13 14:45:19 +0200

    aikatehtävä

---
+++ demo2harjoitus5/demo2harjoitus5/Program.cs
@@ -0,0 +1,36 @@
+﻿//Tee ohjelma, joka näyttää annetun sekuntimäärän tunteina, minuutteina ja sekunteina. Aikamääre sekuntteina kysytään käyttäjältä. 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo1harjoitus4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            Console.Write("montako sekuntia muutetaan");
+           int luku = int.Parse(Console.ReadLine());
+         
+            int luku1 = luku / 3600;
+   
+            int luku2 = (luku- luku1*3600)/60;
+     
+            int luku3 = (luku-luku1*3600 -luku2*60);
+
+
+            Console.WriteLine("tunnit : ");
+            Console.WriteLine(luku1);
+            Console.WriteLine("minuutit : ");
+            Console.WriteLine(luku2);
+            Console.WriteLine("sekuntit : ");
+            Console.WriteLine(luku3);
+        }
+    }
+}
